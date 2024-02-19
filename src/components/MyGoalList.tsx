2f6405b7bc42b7733597a45370a3c64703649bb3
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-02-19 20:24:28 +0530

    Render an info box with some info and warnings

--- src/components/MyGoalList.tsx
+++ src/components/MyGoalList.tsx
@@ -1,4 +1,7 @@
+import { type ReactNode } from 'react';
+
 import { CareerGoal } from '../App.tsx';
+import InfoBox from './InfoBox.tsx';
 import MyGoal from './MyGoal.tsx';
 
 type goalsListProps = {
@@ -7,17 +10,35 @@ type goalsListProps = {
 };
 
 const MyGoalList = function ({ goals, onDeleteGoal }: goalsListProps) {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode='hint'>You have no goals yet! Start setting some!</InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode='warning'>
+        You're collecting a lot of goals! Don't put too much on your plate!
+      </InfoBox>
+    );
+  }
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <MyGoal
-            onDelete={onDeleteGoal}
-            goal={goal}
-          />
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <MyGoal
+              onDelete={onDeleteGoal}
+              goal={goal}
+            />
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
